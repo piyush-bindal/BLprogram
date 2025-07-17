026bdfd2129a586435a4da636e612ac81fe16b2e
piyush-bindal <122973837+piyush-bindal@users.noreply.github.com>
2025-07-17 10:18:54 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+//        for(int i=1;i<=10;i++){
+//            System.out.println(n+"x"+i+"="+n*i);
+//
+//        }
+        int i = 1;
+        while( i<=10){
+            System.out.println(n+"x"+i+"="+n*i);
+            i++;
+
+        }
+    }
+}
